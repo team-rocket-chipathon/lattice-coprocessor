d0e6a6355576fc4224e2776cf3039cab777cafb0
Marcus Mellor <infinitymdm@gmail.com>
2026-06-17 10:30:43 -0500

    keccak: Add interface with modports and fips202 mode enum

---
+++ hw/trng/keccak/fips202.sv
@@ -0,0 +1,10 @@
+package fips202;
+    typedef enum logic [2:0] {
+        SHAKE128,
+        SHAKE256,
+        SHA3_224,
+        SHA3_256,
+        SHA3_384,
+        SHA3_512
+    } mode_t;
+endpackage
